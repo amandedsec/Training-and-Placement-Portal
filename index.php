cec1af72bd5ecfbac4dac628280321cd98dbae82
Aman Singh <aman singh>
2023-02-19 22:01:44 +0530

    project completed

---
+++ index.php
@@ -0,0 +1,214 @@
+<?php
+
+//To Handle Session Variables on This Page
+session_start();
+
+
+//Including Database Connection From db.php file to avoid rewriting in all files
+require_once("db.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<title>Home</title>
+
+<head>
+    <?php
+
+    include 'php/head.php'
+
+    ?>
+
+
+</head>
+
+<body>
+
+    <!-- navbar starts -->
+    <?php
+
+    include 'php/header.php'
+
+    ?> 
+    <!-- header ends -->
+
+    <section id="hero-animated" class="hero-animated d-flex align-items-center">
+        <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative" data-aos="zoom-out">
+            <img src="assets/img/hero-carousel/main.webp" class="img-fluid animated">
+            <h2>Welcome to <span>Placement Cell</span></h2>
+            <p>One step closer to your dream job  by providing you mentorship </p>
+            <div class="d-flex">
+                <a href="login.php" class="btn-get-started scrollto">Get Started</a>
+
+            </div>
+        </div>
+    </section>
+
+    <main id="main">
+
+    
+        
+
+
+
+        <!-- ======= Call To Action Section ======= -->
+        <section id="cta" class="cta">
+            <div class="container" data-aos="zoom-out">
+
+                <div class="row g-5">
+
+                    <div class="col-lg-8 col-md-6 content d-flex flex-column justify-content-center order-last order-md-first">
+                        <h3>Placement <em>Portal</em> </h3>
+                        <p>The Placement Cell plays a crucial role in locating job
+                            opportunities for under graduates and post graduates passing out from the college by
+                            keeping in touch with reputed firms and industrial establishments.
+                            <br>The placement cell operates round the year to facilitate contacts between companies
+                            and graduates. The number of students placed through the campus interviews is
+                            continuously rising.
+                        </p>
+                        <a class="cta-btn align-self-start" href="#">Get Started</a>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 order-first order-md-last d-flex align-items-center">
+                        <div class="img">
+                            <img src="assets/img/describe.webp" alt="" class="img-fluid">
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Call To Action Section -->
+
+
+        <!-- ======= Clients Section ======= -->
+        <section id="clients" class="clients">
+            <div class="container" data-aos="zoom-out">
+
+                <div class="clients-slider swiper">
+                    <div class="swiper-wrapper align-items-center">
+                        <div class="swiper-slide"><img src="assets/img/clients/client-1.svg" class="img-fluid" alt=""></div>
+                        <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+                        <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+                        <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+                        <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
+                        <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
+                        <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+                        <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End Clients Section -->
+
+
+
+        <!-- ======= Features Section ======= -->
+        <section id="objectives" class="features" name="objectives">
+            <div class="container" data-aos="fade-up">
+
+
+
+                <div class="tab-content">
+
+                    <div class="tab-pane active show" id="tab-1">
+                        <div class="row gy-4">
+                            <div class="col-lg-8 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
+                                <h3>Objectives</h3>
+                                <p class="fst-itali">
+                                    Our Placement Portal serves various objectives:
+                                </p>
+                                <ul>
+                                    <li><i class="bi bi-check-circle-fill"></i> Discover new opportunities, learn and practice on the go, prepare better for interviews
+                                    </li>
+                                    <li><i class="bi bi-check-circle-fill"></i> To motivate students to develop Technical knowledge and soft skills in
+                                        terms of career planning, goal setting.
+                                    </li>
+                                    <li><i class="bi bi-check-circle-fill"></i> Receive reliable information notifications, apply on the go via mobile apps, and keep better track of interview dates.</li>
+
+                                </ul>
+                            </div>
+                            <div class="col-lg-4 order-1 order-lg-2 text-center" data-aos="fade-up" data-aos-delay="200">
+                                <img src="assets/img/objective.jpg" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Tab Content -->
+
+
+
+
+
+                
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+
+    <?php
+
+    include 'php/footer.php';
+    ?>
+
+    <!-- End Footer -->
+
+    <!-- TPO bot -->
+
+    
+
+  
+
+    
+
+    </div>
+
+    <!-- JS FILES -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="{% static 'js/tabs.js' %}"></script>
+    <style>
+
+    </style>
+    <script>
+        // ===== Scroll to Top ==== 
+        $(window).scroll(function() {
+            if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+                $('#return-to-top').fadeIn(200); // Fade in the arrow
+            } else {
+                $('#return-to-top').fadeOut(200); // Else fade out the arrow
+            }
+        });
+        $('#return-to-top').click(function() { // When arrow is clicked
+            $('body,html').animate({
+                scrollTop: 0 // Scroll to top of body
+            }, 500);
+        });
+
+       
+
+        function closeForm() {
+            document.getElementById("myForm").style.display = "none";
+        }
+        $("#button").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#about").offset().top
+            }, 1000);
+        });
+        $("#button1").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#events").offset().top
+            }, 1000);
+        });
+    </script>
+    <script src="assets/js/main1.js"></script>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <script src="assets/js/counter.js"></script>
+
+
+
+    <!-- tpo bot ends -->
+
+
+
+</body>
+
+</html>
